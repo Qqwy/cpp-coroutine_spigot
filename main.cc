21721e13d366f448198bd35c5cdd92c37f472e16
Marten/Qqwy <w-m@wmcode.nl>
2021-11-14 15:03:22 +0100

    WIP

--- main.cc
+++ main.cc
@@ -1,6 +1,35 @@
 #include "generator.hh"
 #include <iostream>
 
+
+struct LFT {
+  double q;
+  double r;
+  double s;
+  double t;
+};
+
+double extr(LFT lft, double x) {
+  return (lft.q * x + lft.r) / (lft.s * x + lft.t);
+}
+
+LFT unit() {
+  return {1, 0, 0, 1};
+}
+
+LFT comp(LFT a, LFT b) {
+  auto q = a.q;
+  auto r = a.r;
+  auto s = a.s;
+  auto t = a.t;
+
+  auto u = b.q;
+  auto v = b.r;
+  auto w = b.s;
+  auto x = b.t;
+  return {q * u + r * w, q * v + r * x, s *u + t * w, s * v + t * x};
+}
+
 template <typename B, typename C>
 using NextFun = C (*)(B);
 template <typename B, typename C>
@@ -35,4 +64,43 @@ cppcoro::generator<C> stream(NextFun<B, C> next, SafeFun<B, C> safe,
   }
 }
 
-int main() { std::cout << "Boom\n"; }
+#include <cmath>
+
+cppcoro::generator<double> positive_doubleegers() {
+  double num = 1;
+  while (true) {
+    co_yield num;
+    ++num;
+  }
+}
+
+cppcoro::generator<LFT> pi_lfts() {
+  return cppcoro::fmap([](auto k){
+    return LFT{k, 4 * k + 2, 0, 2 * k + 1};
+  }, positive_doubleegers());
+}
+
+cppcoro::generator<double> pi() {
+  auto init = unit();
+  auto lfts = pi_lfts();
+
+  auto next = [](LFT z) { return std::floor(extr(z, 3));};
+  auto safe = [](LFT z, double n){ return n == std::floor(extr(z, 4)); };
+  auto prod = [](LFT z, double n){ return comp({10, -10 * n, 0, 1}, z); };
+  auto cons = [](LFT z, LFT z2){ return comp(z, z2); };
+  return stream(*next, *safe, *prod, *cons, init, std::move(lfts));
+}
+
+int main()
+{
+  auto count = 0;
+
+  for (auto digit : pi()) {
+    std::cerr << digit;
+
+    if(count > 1000) {
+      break;
+    }
+    ++count;
+  }
+}
